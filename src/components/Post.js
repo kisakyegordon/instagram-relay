e005aab2ca281f5dfcef1f2cd11740bdb59489d5
kisakyegordon <gordon56692@gmail.com>
2019-04-17 17:10:23 +0300

    first commit

---
+++ src/components/Post.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+class Post extends React.Component {
+
+    _handleDelete = () => {
+
+    }
+
+    render() {
+        return (
+            <div className="pa3 bg-black-05 ma3">
+                <div
+                 className="w-100"
+                 style={{
+                     backgroundImage: `url(${this.props.post.imageUrl})`,
+                     backgroundSize: 'cover',
+                     paddingBottom: '100%'
+                 }}
+                 />
+                 <div className="pt3">
+                    {this.props.post.description}&nbsp;
+                    <span className="red f6 pointer dim" onClick={this._handleDelete}>Delete</span>
+                 </div>
+            </div>
+        )
+    }
+}
+
+export default Post;
